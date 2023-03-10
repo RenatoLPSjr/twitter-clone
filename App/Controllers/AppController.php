7b9c590005aedd8024c268a48c8e3c6a5e4d524f
Renato Lopes <renato.lop.jr@gmail.com>
2023-03-10 10:51:56 -0300

    Adicionado sistema de tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -10,13 +10,44 @@ class AppController extends Action
 {
     public function timeline()
     {
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $this->view->tweets = $tweet->getAll();
+
+        $this->render('timeline');
+
+    }
+    public function tweet()
+    {
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweet->salvar();
+
+        header('Location: /timeline');
+
+    }
+
+    public function validaAutenticacao()
+    {
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['nome'] != '')
+        if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '')
         {
-            $this->render('timeline');
-        }else{
             header('Location: /?login=erro');
         }
+    }
+
+    public function quemSeguir()
+    {
+        $this->validaAutenticacao();
+        $this->render('quemSeguir');
     }
 }
